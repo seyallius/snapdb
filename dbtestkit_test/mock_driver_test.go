seyallius/gopherbox

feat(dbtestkit): add ORM-agnostic database test container toolkit
Why: Integration tests need real databases to verify SQL correctness,
but running tests against live databases is slow, non-hermetic, and
hard to parallelize. The existing casdoor test utilities were tightly
coupled to xorm and beego, making them impossible to reuse in other
projects or with different ORMs.

How: Implements a generic test container toolkit with three backends
(MySQL 8.x, PostgreSQL 16, SQLite), functional-options configuration
(no environment variables), and an ORM-agnostic Engine interface.
The toolkit uses a "fast path / slow path" strategy: the first run
generates a pristine SQL dump (~8s), subsequent resets restore it in
milliseconds. A mutex serializes resets so parallel tests safely share
one container. Includes an xorm adapter and complete test coverage.

Key changes:
- Add `dbtestkit` package with lifecycle orchestration (`Run`/`Reset`)
- Implement MySQL, PostgreSQL, and SQLite drivers with tmpfs and
  optimized entrypoints (MySQL pre-baked tarball support)
- Define minimal `Engine` interface (Exec, QueryString, Ping, etc.)
- Provide xorm adapter in `xormadapter` subpackage
- Add comprehensive unit/integration tests with mock driver
- Update `go.mod` with testcontainers-go, xorm, and SQLite dependencies

dbtestkit/dbtestkit_test/mock_driver_test.go
@@ -1,0 +1,81 @@
+// Package dbtestkit_test. mock_driver_test.go - A fake DatabaseDriver
+// implementation used by the unit tests to exercise the lifecycle without
+// spinning up Docker.
+package dbtestkit_test
+
+import (
+	"context"
+	"sync"
+
+	"github.com/seyallius/gopherbox/dbtestkit"
+)
+
+// ---------------------------------- Types, Variables & Constants ---------------------------------- //
+
+// mockDriver is a recording DatabaseDriver that performs no real I/O.
+// Every method call appends an entry to Calls so tests can assert ordering.
+type mockDriver struct {
+	mu    sync.Mutex
+	calls []string
+	dsn   string
+}
+
+// ------------------------------------------- Constructor(s) --------------------------------------- //
+
+// newMockDriver returns a fresh mockDriver.
+func newMockDriver(dsn string) *mockDriver {
+	return &mockDriver{dsn: dsn}
+}
+
+// -------------------------------------------- Public API ------------------------------------------ //
+
+// Driver implements dbtestkit.DatabaseDriver.
+func (m *mockDriver) Driver() dbtestkit.Driver { return dbtestkit.DriverSQLite }
+
+// Start implements dbtestkit.DatabaseDriver.
+func (m *mockDriver) Start(_ context.Context, _ *dbtestkit.Environment) (string, error) {
+	m.record("Start")
+	return m.dsn, nil
+}
+
+// RestoreDump implements dbtestkit.DatabaseDriver.
+func (m *mockDriver) RestoreDump(_ context.Context, _ *dbtestkit.Environment, _ string) error {
+	m.record("RestoreDump")
+	return nil
+}
+
+// GenerateDump implements dbtestkit.DatabaseDriver.
+func (m *mockDriver) GenerateDump(_ context.Context, _ *dbtestkit.Environment, _ string) error {
+	m.record("GenerateDump")
+	return nil
+}
+
+// Truncate implements dbtestkit.DatabaseDriver.
+func (m *mockDriver) Truncate(_ context.Context, _ *dbtestkit.Environment) error {
+	m.record("Truncate")
+	return nil
+}
+
+// Stop implements dbtestkit.DatabaseDriver.
+func (m *mockDriver) Stop(_ context.Context, _ *dbtestkit.Environment) error {
+	m.record("Stop")
+	return nil
+}
+
+// ------------------------------------------- Internal Helpers ------------------------------------- //
+
+// record appends a method name to the call log.
+func (m *mockDriver) record(name string) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.calls = append(m.calls, name)
+}
+
+// Calls returns a snapshot of the recorded call sequence.
+func (m *mockDriver) Calls() []string {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	out := make([]string, len(m.calls))
+	copy(out, m.calls)
+	return out
+}

dbtestkit/doc.go
@@ -1,0 +1,13 @@
+// Package dbtestkit. doc.go - Provides a generic, ORM-agnostic test container toolkit
+// for integration tests that need a real database (MySQL, PostgreSQL, or SQLite).
+//
+// dbtestkit is designed to be wired into a Go test package's TestMain function. It boots
+// a database (in Docker for MySQL/PostgreSQL, or on local disk for SQLite), restores a
+// pre-baked pristine SQL dump for millisecond resets between tests, and exposes a single
+// Reset entry-point that returns the database to a known-good state before every test.
+//
+// Configuration is entirely functional-options based — no environment variables. Every
+// project-specific concern (schema creation, data seeding, engine construction, cache
+// invalidation) is supplied by the caller via callbacks, so the library never imports
+// your ORM or your domain objects.
+package dbtestkit

dbtestkit/logger.go
@@ -1,0 +1,68 @@
+// Package dbtestkit. logger.go - Provides the Logger interface and a default
+// tree-style printer that mirrors the original casdoor test output format.
+package dbtestkit
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+// ---------------------------------- Types, Variables & Constants ---------------------------------- //
+
+// Logger is the logging surface used by the library. Implementations are free to
+// forward to *testing.T, log/slog, zap, or any other sink.
+type Logger interface {
+	// Step reports the elapsed time of an inner setup/reset step.
+	Step(name string, elapsed time.Duration)
+
+	// End reports the elapsed time of an outer setup/reset phase.
+	End(name string, elapsed time.Duration)
+
+	// Info emits an informational line (e.g. "==> 🚀 Test Container Setup").
+	Info(msg string)
+
+	// Warn emits a non-fatal warning.
+	Warn(msg string)
+}
+
+// -------------------------------------------- Public API ------------------------------------------ //
+
+// DefaultLogger prints tree-style timing output to stdout, matching the format used
+// by the original casdoor test utilities:
+//
+//	===> 🚀 Test Container Setup
+//	    ├── Create MySQL Container:                420ms
+//	    └── ✅ Test Container Ready!               1.2s
+type DefaultLogger struct {
+	w io.Writer
+}
+
+// NewDefaultLogger returns a DefaultLogger writing to w. Pass nil to use os.Stdout.
+func NewDefaultLogger(w io.Writer) *DefaultLogger {
+	if w == nil {
+		w = os.Stdout
+	}
+	return &DefaultLogger{w: w}
+}
+
+// Step implements Logger.
+func (l *DefaultLogger) Step(name string, elapsed time.Duration) {
+	fmt.Fprintf(l.w, "\t├── %-35s %v\n", name+":", elapsed.Round(time.Millisecond))
+}
+
+// End implements Logger.
+func (l *DefaultLogger) End(name string, elapsed time.Duration) {
+	fmt.Fprintf(l.w, "\t└── ✅ %-32s %v\n\n", name, elapsed.Round(time.Millisecond))
+}
+
+// Info implements Logger.
+func (l *DefaultLogger) Info(msg string) {
+	fmt.Fprintf(l.w, "\n===> %s\n", msg)
+}
+
+// Warn implements Logger.
+func (l *DefaultLogger) Warn(msg string) {
+	fmt.Fprintf(l.w, "⚠️  Warning: %s\n", msg)
+}

dbtestkit/testing.go
@@ -1,0 +1,136 @@
+// Package dbtestkit. testing.go - Exposes a minimal set of internal hooks for
+// use by tests in this package and downstream consumers that need to assert
+// option application without spinning up a real container.
+//
+// This file ships with the library (it is not a _test.go file) so the
+// functions it exposes are part of the public API. They are clearly named
+// "ForTesting" to discourage production use.
+package dbtestkit
+
+import "context"
+
+// -------------------------------------------- Public API (Testing Only) --------------------------------------------
+
+// TestingConfig is a read-only view of the internal config struct, exposed
+// for tests that need to assert what options were applied. Production code
+// should never use this type.
+type TestingConfig struct {
+	DriverName       Driver
+	Database         DatabaseConfig
+	SchemaInit       SchemaInitializer
+	DataInit         DataInitializer
+	EngineInit       EngineInitializer
+	CacheInvalidator CacheInvalidator
+	Seeders          []Seeder
+	PristineDumpPath string
+	TestdataDir      string
+	SQLitePath       string
+	ProjectRoot      string
+	GeneratePristine bool
+	Logger           Logger
+}
+
+// ApplyOptionsForTesting builds a TestingConfig from a list of options.
+//
+// This is the only sanctioned way for tests to inspect option application
+// without reimplementing the validation logic. Production callers should
+// use Run, which performs the same validation internally.
+//
+// Returns an error if the options fail validation (missing required fields,
+// nil callbacks, etc.).
+func ApplyOptionsForTesting(opts ...Option) (*TestingConfig, error) {
+	cfg, err := applyOptions(opts)
+	if err != nil {
+		return nil, err
+	}
+	return &TestingConfig{
+		DriverName:       cfg.driverName,
+		Database:         cfg.database,
+		SchemaInit:       cfg.schemaInit,
+		DataInit:         cfg.dataInit,
+		EngineInit:       cfg.engineInit,
+		CacheInvalidator: cfg.cacheInvalidator,
+		Seeders:          cfg.seeders,
+		PristineDumpPath: cfg.pristineDumpPath,
+		TestdataDir:      cfg.testdataDir,
+		SQLitePath:       cfg.sqlitePath,
+		ProjectRoot:      cfg.projectRoot,
+		GeneratePristine: cfg.generatePristine,
+		Logger:           cfg.logger,
+	}, nil
+}
+
+// SetupForTesting runs the package-internal setup function with a config
+// derived from the given options. It returns the constructed Environment,
+// Engine, and the resolved DatabaseDriver, exactly as the production Run
+// function would see them.
+//
+// Tests use this to exercise the fast/slow-path logic without invoking
+// os.Exit (which Run does and which makes direct testing impossible).
+func SetupForTesting(opts ...Option) (*Environment, Engine, DatabaseDriver, error) {
+	cfg, err := applyOptions(opts)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	ctx := context.Background()
+	return setup(ctx, cfg)
+}
+
+// ResetForTesting runs the package-internal resetDatabase function against
+// a runtime state assembled from the given options + a previously-returned
+// Environment/Engine/Driver triple. It does NOT call os.Exit and is safe
+// to use from test code.
+//
+// seeders and cacheInvalidator override whatever was supplied via opts.
+func ResetForTesting(
+	t MinimalTandB,
+	opts ...Option,
+) error {
+	cfg, err := applyOptions(opts)
+	if err != nil {
+		return err
+	}
+	ctx := context.Background()
+	env, eng, drv, err := setup(ctx, cfg)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = teardown(ctx, env, eng, drv) }()
+
+	rt := &runtimeState{cfg: cfg, env: env, engine: eng, drv: drv}
+	return resetDatabase(t, rt)
+}
+
+// NewEnvironmentForTesting constructs an Environment with the minimal fields
+// a driver needs to operate (driver name, testdata dir, sqlite path).
+//
+// If base is non-nil, its other fields (DSN, project root, logger) are
+// inherited; otherwise sensible defaults are used. This is intended for
+// driver-level tests that want to exercise Start/Stop/RestoreDump/Truncate
+// in isolation without invoking the full lifecycle.
+func NewEnvironmentForTesting(base *Environment, driverName Driver, testdataDir, sqlitePath string) *Environment {
+	env := &Environment{
+		driver:      driverName,
+		testdataDir: testdataDir,
+		sqlitePath:  sqlitePath,
+		logger:      NewDefaultLogger(nil),
+	}
+	if base != nil {
+		env.database = base.database
+		env.dsn = base.dsn
+		env.projectRoot = base.projectRoot
+		env.ctx = base.ctx
+		env.engine = base.engine
+	}
+	return env
+}
+
+// EnvWithEngineForTesting returns a shallow copy of env with the Engine
+// field replaced. Used by driver tests to inject a test-specific Engine
+// before calling Truncate.
+func EnvWithEngineForTesting(env *Environment, eng Engine) *Environment {
+	if env == nil {
+		return nil
+	}
+	return env.withEngine(eng)
+}

seyallius/snapdb

dbtestkit_test/mock_driver_test.go
@@ -1,0 +1,81 @@
+// Package snapdb_test. mock_driver_test.go - A fake DatabaseDriver
+// implementation used by the unit tests to exercise the lifecycle without
+// spinning up Docker.
+package snapdb_test
+
+import (
+	"context"
+	"sync"
+
+	"github.com/seyallius/snapdb"
+)
+
+// ---------------------------------- Types, Variables & Constants ---------------------------------- //
+
+// mockDriver is a recording DatabaseDriver that performs no real I/O.
+// Every method call appends an entry to Calls so tests can assert ordering.
+type mockDriver struct {
+	mu    sync.Mutex
+	calls []string
+	dsn   string
+}
+
+// ------------------------------------------- Constructor(s) --------------------------------------- //
+
+// newMockDriver returns a fresh mockDriver.
+func newMockDriver(dsn string) *mockDriver {
+	return &mockDriver{dsn: dsn}
+}
+
+// -------------------------------------------- Public API ------------------------------------------ //
+
+// Driver implements dbtestkit.DatabaseDriver.
+func (m *mockDriver) Driver() dbtestkit.Driver { return dbtestkit.DriverSQLite }
+
+// Start implements dbtestkit.DatabaseDriver.
+func (m *mockDriver) Start(_ context.Context, _ *dbtestkit.Environment) (string, error) {
+	m.record("Start")
+	return m.dsn, nil
+}
+
+// RestoreDump implements dbtestkit.DatabaseDriver.
+func (m *mockDriver) RestoreDump(_ context.Context, _ *dbtestkit.Environment, _ string) error {
+	m.record("RestoreDump")
+	return nil
+}
+
+// GenerateDump implements dbtestkit.DatabaseDriver.
+func (m *mockDriver) GenerateDump(_ context.Context, _ *dbtestkit.Environment, _ string) error {
+	m.record("GenerateDump")
+	return nil
+}
+
+// Truncate implements dbtestkit.DatabaseDriver.
+func (m *mockDriver) Truncate(_ context.Context, _ *dbtestkit.Environment) error {
+	m.record("Truncate")
+	return nil
+}
+
+// Stop implements dbtestkit.DatabaseDriver.
+func (m *mockDriver) Stop(_ context.Context, _ *dbtestkit.Environment) error {
+	m.record("Stop")
+	return nil
+}
+
+// ------------------------------------------- Internal Helpers ------------------------------------- //
+
+// record appends a method name to the call log.
+func (m *mockDriver) record(name string) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.calls = append(m.calls, name)
+}
+
+// Calls returns a snapshot of the recorded call sequence.
+func (m *mockDriver) Calls() []string {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	out := make([]string, len(m.calls))
+	copy(out, m.calls)
+	return out
+}

doc.go
@@ -1,0 +1,13 @@
+// Package dbtestkit. doc.go - Provides a generic, ORM-agnostic test container toolkit
+// for integration tests that need a real database (MySQL, PostgreSQL, or SQLite).
+//
+// dbtestkit is designed to be wired into a Go test package's TestMain function. It boots
+// a database (in Docker for MySQL/PostgreSQL, or on local disk for SQLite), restores a
+// pre-baked pristine SQL dump for millisecond resets between tests, and exposes a single
+// Reset entry-point that returns the database to a known-good state before every test.
+//
+// Configuration is entirely functional-options based — no environment variables. Every
+// project-specific concern (schema creation, data seeding, engine construction, cache
+// invalidation) is supplied by the caller via callbacks, so the library never imports
+// your ORM or your domain objects.
+package snapdb

logger.go
@@ -1,0 +1,68 @@
+// Package dbtestkit. logger.go - Provides the Logger interface and a default
+// tree-style printer that mirrors the original casdoor test output format.
+package snapdb
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+// ---------------------------------- Types, Variables & Constants ---------------------------------- //
+
+// Logger is the logging surface used by the library. Implementations are free to
+// forward to *testing.T, log/slog, zap, or any other sink.
+type Logger interface {
+	// Step reports the elapsed time of an inner setup/reset step.
+	Step(name string, elapsed time.Duration)
+
+	// End reports the elapsed time of an outer setup/reset phase.
+	End(name string, elapsed time.Duration)
+
+	// Info emits an informational line (e.g. "==> 🚀 Test Container Setup").
+	Info(msg string)
+
+	// Warn emits a non-fatal warning.
+	Warn(msg string)
+}
+
+// -------------------------------------------- Public API ------------------------------------------ //
+
+// DefaultLogger prints tree-style timing output to stdout, matching the format used
+// by the original casdoor test utilities:
+//
+//	===> 🚀 Test Container Setup
+//	    ├── Create MySQL Container:                420ms
+//	    └── ✅ Test Container Ready!               1.2s
+type DefaultLogger struct {
+	w io.Writer
+}
+
+// NewDefaultLogger returns a DefaultLogger writing to w. Pass nil to use os.Stdout.
+func NewDefaultLogger(w io.Writer) *DefaultLogger {
+	if w == nil {
+		w = os.Stdout
+	}
+	return &DefaultLogger{w: w}
+}
+
+// Step implements Logger.
+func (l *DefaultLogger) Step(name string, elapsed time.Duration) {
+	fmt.Fprintf(l.w, "\t├── %-35s %v\n", name+":", elapsed.Round(time.Millisecond))
+}
+
+// End implements Logger.
+func (l *DefaultLogger) End(name string, elapsed time.Duration) {
+	fmt.Fprintf(l.w, "\t└── ✅ %-32s %v\n\n", name, elapsed.Round(time.Millisecond))
+}
+
+// Info implements Logger.
+func (l *DefaultLogger) Info(msg string) {
+	fmt.Fprintf(l.w, "\n===> %s\n", msg)
+}
+
+// Warn implements Logger.
+func (l *DefaultLogger) Warn(msg string) {
+	fmt.Fprintf(l.w, "⚠️  Warning: %s\n", msg)
+}

testing.go
@@ -1,0 +1,136 @@
+// Package dbtestkit. testing.go - Exposes a minimal set of internal hooks for
+// use by tests in this package and downstream consumers that need to assert
+// option application without spinning up a real container.
+//
+// This file ships with the library (it is not a _test.go file) so the
+// functions it exposes are part of the public API. They are clearly named
+// "ForTesting" to discourage production use.
+package snapdb
+
+import "context"
+
+// -------------------------------------------- Public API (Testing Only) --------------------------------------------
+
+// TestingConfig is a read-only view of the internal config struct, exposed
+// for tests that need to assert what options were applied. Production code
+// should never use this type.
+type TestingConfig struct {
+	DriverName       Driver
+	Database         DatabaseConfig
+	SchemaInit       SchemaInitializer
+	DataInit         DataInitializer
+	EngineInit       EngineInitializer
+	CacheInvalidator CacheInvalidator
+	Seeders          []Seeder
+	PristineDumpPath string
+	TestdataDir      string
+	SQLitePath       string
+	ProjectRoot      string
+	GeneratePristine bool
+	Logger           Logger
+}
+
+// ApplyOptionsForTesting builds a TestingConfig from a list of options.
+//
+// This is the only sanctioned way for tests to inspect option application
+// without reimplementing the validation logic. Production callers should
+// use Run, which performs the same validation internally.
+//
+// Returns an error if the options fail validation (missing required fields,
+// nil callbacks, etc.).
+func ApplyOptionsForTesting(opts ...Option) (*TestingConfig, error) {
+	cfg, err := applyOptions(opts)
+	if err != nil {
+		return nil, err
+	}
+	return &TestingConfig{
+		DriverName:       cfg.driverName,
+		Database:         cfg.database,
+		SchemaInit:       cfg.schemaInit,
+		DataInit:         cfg.dataInit,
+		EngineInit:       cfg.engineInit,
+		CacheInvalidator: cfg.cacheInvalidator,
+		Seeders:          cfg.seeders,
+		PristineDumpPath: cfg.pristineDumpPath,
+		TestdataDir:      cfg.testdataDir,
+		SQLitePath:       cfg.sqlitePath,
+		ProjectRoot:      cfg.projectRoot,
+		GeneratePristine: cfg.generatePristine,
+		Logger:           cfg.logger,
+	}, nil
+}
+
+// SetupForTesting runs the package-internal setup function with a config
+// derived from the given options. It returns the constructed Environment,
+// Engine, and the resolved DatabaseDriver, exactly as the production Run
+// function would see them.
+//
+// Tests use this to exercise the fast/slow-path logic without invoking
+// os.Exit (which Run does and which makes direct testing impossible).
+func SetupForTesting(opts ...Option) (*Environment, Engine, DatabaseDriver, error) {
+	cfg, err := applyOptions(opts)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	ctx := context.Background()
+	return setup(ctx, cfg)
+}
+
+// ResetForTesting runs the package-internal resetDatabase function against
+// a runtime state assembled from the given options + a previously-returned
+// Environment/Engine/Driver triple. It does NOT call os.Exit and is safe
+// to use from test code.
+//
+// seeders and cacheInvalidator override whatever was supplied via opts.
+func ResetForTesting(
+	t MinimalTandB,
+	opts ...Option,
+) error {
+	cfg, err := applyOptions(opts)
+	if err != nil {
+		return err
+	}
+	ctx := context.Background()
+	env, eng, drv, err := setup(ctx, cfg)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = teardown(ctx, env, eng, drv) }()
+
+	rt := &runtimeState{cfg: cfg, env: env, engine: eng, drv: drv}
+	return resetDatabase(t, rt)
+}
+
+// NewEnvironmentForTesting constructs an Environment with the minimal fields
+// a driver needs to operate (driver name, testdata dir, sqlite path).
+//
+// If base is non-nil, its other fields (DSN, project root, logger) are
+// inherited; otherwise sensible defaults are used. This is intended for
+// driver-level tests that want to exercise Start/Stop/RestoreDump/Truncate
+// in isolation without invoking the full lifecycle.
+func NewEnvironmentForTesting(base *Environment, driverName Driver, testdataDir, sqlitePath string) *Environment {
+	env := &Environment{
+		driver:      driverName,
+		testdataDir: testdataDir,
+		sqlitePath:  sqlitePath,
+		logger:      NewDefaultLogger(nil),
+	}
+	if base != nil {
+		env.database = base.database
+		env.dsn = base.dsn
+		env.projectRoot = base.projectRoot
+		env.ctx = base.ctx
+		env.engine = base.engine
+	}
+	return env
+}
+
+// EnvWithEngineForTesting returns a shallow copy of env with the Engine
+// field replaced. Used by driver tests to inject a test-specific Engine
+// before calling Truncate.
+func EnvWithEngineForTesting(env *Environment, eng Engine) *Environment {
+	if env == nil {
+		return nil
+	}
+	return env.withEngine(eng)
+}